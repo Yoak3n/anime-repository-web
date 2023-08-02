a31881a7e1dac193d706b1771925c00f9aaa69fb
Yoake <3354590676@qq.com>
2023-08-02 23:41:21 +0800

    fix: directory don't exist

--- backend/database/sqlite.go
+++ backend/database/sqlite.go
@@ -3,11 +3,13 @@ package database
 import (
 	"fmt"
 	"github/Yoak3n/anime-repository-web/package/logger"
+	"github/Yoak3n/anime-repository-web/package/util"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
 func initSqlite() *gorm.DB {
+	util.CreateDirNotExists("data/db")
 	dsn := fmt.Sprintf("data/db/sqlite.db")
 	db, err := gorm.Open(sqlite.Open(dsn), &gorm.Config{})
 	if err != nil {
